130f8f864d52d8e9020e6f22a7dd07e69a59eda6
VIMOKSH <vemurivimokshram@gmail.com>
2026-04-30 13:53:46 +0530

    Hardcode Nodemailer to use IPv4 via family property to permanently fix ENETUNREACH

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -19,6 +19,7 @@ const sendEmail = async (options) => {
             host: 'smtp.gmail.com',
             port: 465,
             secure: true,
+            family: 4,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
